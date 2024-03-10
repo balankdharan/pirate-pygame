a2e8d37943c56c8b44bd55aec2b269f2cd028497
balankdharan <balankdharan@gmail.com>
2024-03-10 19:06:21 +0530

    added sprites with tiles in tmx

--- code/main.py
+++ code/main.py
@@ -1,13 +1,16 @@
 from settings import *
 from level import Level
+from pytmx.util_pygame import load_pygame
+from os.path import join, dirname
 
 class Game:
     def __init__(self):
         pygame.init()
         self.display_surafce= pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Super pirate World")
+        self.tmx_maps ={0:load_pygame(join('..','data','levels','omni.tmx'))}
 
-        self.current_stage= Level()
+        self.current_stage= Level(self.tmx_maps[0])
     def run(self):
         while True:
             for event in pygame.event.get():
